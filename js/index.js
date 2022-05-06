83b0102e1cb38d4db9f1dbaeee13231cfe0d72af
Harveer Hans <harveerhans59@gmail.com>
2022-05-05 21:16:49 -0400

    fizzBuzz App component completed

--- js/index.js
+++ js/index.js
@@ -1,60 +1,51 @@
 document.getElementById("btnSubmit").addEventListener("click",getValues);
-//get the values from the page
-//starts or controller function
+
+//extract values from input fields
+//parse values
+//starts and calls the required functions 
 function getValues(){
-    //get values from the page
-    let startValue = document.getElementById('startNumber').value;
-    let endValue = document.getElementById('endNumber').value;
-    let submitButton = document.getElementById('submitButton');
 
-    //parse Into integers
-    startValue=parseInt(startValue);
-    endValue = parseInt(endValue);
+    let fizz = document.getElementById('fizz').value;
+    let buzz = document.getElementById('buzz').value;
+   
+    fizz=parseInt(fizz);
+    buzz=parseInt(buzz);
 
-    //testing if input is valid
-    if(startValue>=endValue){
-        alert("End value must be greater than start value!");
-    }
-    else if(Number.isInteger(startValue) && Number.isInteger(endValue)){
-        //we call generateNumbers
-        let numbers = generateNumbers(startValue, endValue);
-        displayNumbers(numbers);
-    }else{
-        alert("Only enter numbers!");
-    }
-
-    //we call displayNumbers
-    
+    let numbers = generateNumbers(fizz,buzz);
+    displayTable(numbers);
 }
 
-//generate numbers from start value to the endvalue
-//logic function
-function generateNumbers(startValue, endValue){
-    let numbers=[];
-    for(let index=startValue; index<=endValue; index++){
-        numbers.push(index);
+//generate array with numbers and declaring what indexes will be fizz or buzz
+function generateNumbers(fizz, buzz){
+    let numbers = [];
+
+    for(let index=1 ;index<=100;index++){
+        if(index % buzz==0 && index % fizz==0){
+            numbers.push("FIZZBUZZ");
+        }
+        else if(index % fizz==0){
+            numbers.push("FIZZ");
+        }else if(index % buzz==0){
+            numbers.push("BUZZ");
+        }else{
+            numbers.push(index);
+        }
     }
     return numbers;
 }
 
-//display the numbers and mark even numbers bold
-//display or view function
-function displayNumbers(numbers){
-
-    let templateRows="";
-
-    for (let index = 0;index <numbers.length; index++){
-        //classValue is to decide background color of the values based on if they're even or odd
-        let classValue="";
+//display the array in a table format
+function displayTable(numbers){
+    let displayContent="";
     
-        let number= numbers[index];
 
-        if(number % 2 == 0){
-            classValue="table-secondary";
-        }else{
-            classValue="";
+    for(let index=0;index<numbers.length; index++){
+        if(index%5==0){
+            displayContent+=`</tr><tr>`;
         }
-        templateRows += `<tr class="${classValue}"><td>${number}</td></tr>`;
-    }
-    document.getElementById("resultsTable").innerHTML = templateRows;
+        displayContent+= `<td>${numbers[index]}</td>`;
+}
+            
+    
+    document.getElementById('resultsTable').innerHTML= displayContent;
 }
